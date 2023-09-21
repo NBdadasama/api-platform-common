39443d7d08a03d18e6bf811ab555eced9a76c318
NBdadasama <chen123asd@foxmail.com>
2023-09-22 01:25:20 +0800

    模块联通

--- src/main/java/com/alexg/apicommon/model/vo/InterfaceInfoVO.java
+++ src/main/java/com/alexg/apicommon/model/vo/InterfaceInfoVO.java
@@ -12,8 +12,7 @@ import java.util.Date;
 /**
  * 帖子视图
  *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @from <a href="https://yupi.icu">编程导航知识星球</a>
+
  */
 @Data
 public class InterfaceInfoVO implements Serializable {
